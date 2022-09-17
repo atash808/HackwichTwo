2410db39eea4aa865dd1841e4fc7c6d41619f2b0
atash808 <atash@hawaii.edu>
2022-09-17 13:08:02 -1000

    Added a button to the viewcontroller

--- HackwichTwo/ViewController.swift
+++ HackwichTwo/ViewController.swift
@@ -4,6 +4,7 @@
 //
 //  Created by Atash Imanverdi on 9/16/22.
 //
+//
 
 import UIKit
 
@@ -11,9 +12,25 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        //this is the code block for func viewDidLoad
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func pressMeButtonPressed(_ sender: Any) {
+    
+    print("Hello World")
+    
+    }
+    
+    
+    
+    
+    
+    
+    
+    
 
 }
+
+
 
